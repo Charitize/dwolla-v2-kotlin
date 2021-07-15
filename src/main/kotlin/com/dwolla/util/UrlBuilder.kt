f384b7935bcf822957e4afa7c7c3e409fd15d08a
ykravchuk <ykravchuk@lohika.com>
2021-07-15 16:13:47 +0300

    Change URL protocol depending on the baseUrl protocol

--- src/main/kotlin/com/dwolla/util/UrlBuilder.kt
+++ src/main/kotlin/com/dwolla/util/UrlBuilder.kt
@@ -3,18 +3,31 @@ package com.dwolla.util
 import com.dwolla.Environment
 
 internal class UrlBuilder(private val environment: Environment) {
+    private val apiDomain = "^(http://|https://)(.+)(/.*)?\$".toRegex()
+            .find(environment.apiBaseUrl())!!
+            .destructured.component2()
+    private val isUseSsl = environment.apiBaseUrl().startsWith("https://")
 
     fun buildUrl(vararg parts: String): String =
         parts.fold(environment.apiBaseUrl()) { url, p ->
             when {
-                startsWithApiUrl(p) -> trimSlashes(p)
+                hasApiUrlIgnoringProtocol(p) -> convertUrl(p, isUseSsl)
                 p.contains(":") -> throw IllegalArgumentException("Invalid host ($p). Must start with ${environment.apiBaseUrl()}")
-                else -> "$url/${trimSlashes(p)}"
+                else -> "${convertUrl(url, isUseSsl)}/${trimSlashes(p)}"
             }
         }
 
-    private fun startsWithApiUrl(p: String) =
-            p == environment.apiBaseUrl() || p.startsWith("${environment.apiBaseUrl()}/")
+    private fun hasApiUrlIgnoringProtocol(p: String) =
+            p == environment.apiBaseUrl() || p.matches("^(http://|https://)(${this.apiDomain})(/.*)?\$".toRegex())
+
+    private fun convertUrl(p: String, shouldUseSsl: Boolean): String {
+        val trimmed = trimSlashes(p)
+        return if (shouldUseSsl) {
+            trimmed.replace("http://", "https://")
+        } else {
+            trimmed.replace("https://", "http://")
+        }
+    }
 
     private fun trimSlashes(s: String): String =
         s.removePrefix("/").removeSuffix("/")
